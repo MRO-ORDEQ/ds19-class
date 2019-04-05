71eb2168cda3d0697ce9eb690edabe357e0542ed
MRO-ORDEQ <orman.michael@deq.state.or.us>
2019-04-05 13:58:32 -0700

    adding tidyr example

---
+++ code/tidyr.R
@@ -0,0 +1,49 @@
+# Practice with TidyR
+
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(readr)
+
+bikenet <- read_csv("data/bikenet-change.csv")
+summary(factor(bikenet$facility2013))
+
+# how have some of these facilities changed over time?
+
+# gather facility columns into single year variable
+
+colnames(bikenet)
+
+bikenet_long <- bikenet %>% 
+  gather(key = "year", value = "facility",
+         facility2008: facility2013, na.rm = TRUE) %>% 
+  mutate(year = stringr::str_sub(year, start = -4))
+
+head(bikenet_long)  
+
+# -----------------------
+# Danger below! fname can have multiple words!
+# collapse (or unite) street and suffix into one value
+
+bikenet_long <- bikenet_long %>% 
+  unite(col = "street", c("fname", "ftype"), sep = " ")
+
+head(bikenet_long)
+
+# separate street and suffix back to two values
+
+bikenet_long <- bikenet_long %>% 
+  separate(col = street, c("name", "suffix"))
+
+head(bikenet_long)
+
+# ------------------------
+
+bikenet_long %>% filter(bikeid == 139730)
+
+fac_lengths <- bikenet_long %>% 
+  filter(facility %in% c("BKE-LANE", "BKE-BLVD", "BKE-BUFF", 
+                         "BKE-TRAK", "PTH-REMU")) %>% 
+  group_by(year, facility) %>% 
+  summarise(meters = sum(length_m)) %>% 
+  mutate(miles = meters / 1609)
